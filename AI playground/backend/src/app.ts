d216cfa8c00d5bf2a3355b00e03a4903723e9138
Sumit Vishnoi <sumitvishnoi7014@gmail.com>
2026-09-09 21:12:16 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Implemented ChatInterface and ArenaResponse components for user interaction
    - Integrated syntax highlighting with highlight.js and markdown rendering with react-markdown
    - Added user message display component

--- AI playground/backend/src/app.ts
+++ AI playground/backend/src/app.ts
@@ -1,12 +1,31 @@
-import express from "express"
-import runGraph from "./services/graph.ai.js"
+import express from "express";
+import runGraph from "./services/graph.ai.js";
+import cors from "cors";
 
-const app = express()
+const app = express();
+app.use(express.json());
 
-app.get("/", async (req, res)=> {
-   const result = await runGraph("write a program to reverse the linked list")
-   return result
+app.use(cors({
+   origin: "http://localhost:5173",
+   methods: ["POST", "GET"],
+   credentials: true
+}))
 
-})
+app.get("/", async (req, res) => {
+  const result = await runGraph("write a program to reverse the linked list");
+  return result;
+});
+
+app.post("/invoke", async (req, res) => {
+  const { input } = req.body;
+  const result = await runGraph(input);
+
+  res.status(200).json({
+    message: "Graph executed successfully",
+    success: true,
+    result,
+  });
+
+});
 
 export default app;
---
+++ AI playground/frontend/src/app/App.jsx
@@ -0,0 +1,15 @@
+
+import ChatInterface from '../components/ChatInterface'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <ChatInterface />
+    </div>
+  )
+}
+
+export default App
