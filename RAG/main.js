0011dc8dc27ead9a543936828214a9a25fafefeb
Sumit Vishnoi <sumitvishnoi7014@gmail.com>
2026-09-13 23:00:37 +0530

    feat: integrate dotenv and Google Generative AI embeddings for enhanced PDF processing

--- RAG/main.js
+++ RAG/main.js
@@ -1,21 +1,39 @@
-import { PDFParse } from "pdf-parse";
+import dotenv from 'dotenv';
+import { PDFParse } from 'pdf-parse';
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
-import fs from "fs";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import fs from 'fs';
+dotenv.config();
 
-let dataBuffer = fs.readFileSync("./story.pdf");
+let dataBuffer = fs.readFileSync('./story.pdf');
 
-const parse = new PDFParse({
-  data: dataBuffer,
-});
+const parser = new PDFParse({
+    data: dataBuffer
+})
 
-const data = await parse.getText();
+const data = await parser.getText()
+
+const embeddings = new GoogleGenerativeAIEmbeddings({
+    apiKey: process.env.GOOGLE_API_KEY,
+    model: "gemini-embedding-001"
+})
+
 
 const splitter = new RecursiveCharacterTextSplitter({
-  chunkSize: 500,
-  chunkOverlap: 0,
-});
-
+    chunkSize: 500,
+    chunkOverlap: 0,
+})
 
 const chunks = await splitter.splitText(data.text)
 
 console.log(chunks)
+
+const docs = await Promise.all(chunks.map(async (chunk) => {
+    const embedding = await embeddings.embedQuery(chunk)
+    return {
+        text: chunk,
+        embedding
+    }
+}))
+
+console.log(docs)
